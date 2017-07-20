c12f1fba29f094845d846f3167ba215033a53056
Artx/Feinzer <jesusgabriel.91@gmail.com>
2017-07-20 23:29:32 +0000

    Fix Adreno Idler compile error

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -188,10 +188,10 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 				level,
 				priv->bin.total_time,
 				priv->bin.busy_time);
+#endif
 	}
 	priv->bin.total_time = 0;
 	priv->bin.busy_time = 0;
-#endif
 
 	/*
 	 * If the decision is to move to a different level, make sure the GPU
