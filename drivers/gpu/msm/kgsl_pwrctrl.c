f6fb0f8aa9e2f159a6f66a9bfa5d6ddf492b0b8b
Artx/Feinzer <jesusgabriel.91@gmail.com>
2017-07-22 13:12:54 +0000

    GPU activate 27Mhz freq and some changes

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -36,6 +36,8 @@
 #define UPDATE_BUSY_VAL		1000000
 #define UPDATE_BUSY		50
 
+#define DEFAULT_MAX_PWRLEVEL	1
+
 /*
  * Expected delay for post-interrupt processing on A3xx.
  * The delay may be longer, gradually increase the delay
@@ -1154,12 +1156,12 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 		result = -EINVAL;
 		goto done;
 	}
-	pwr->num_pwrlevels = pdata->num_levels;
+	pwr->num_pwrlevels = pdata->num_levels + 1;
 
 	/* Initialize the user and thermal clock constraints */
 
-	pwr->max_pwrlevel = 0;
-	pwr->min_pwrlevel = pdata->num_levels - 2;
+	pwr->max_pwrlevel = DEFAULT_MAX_PWRLEVEL; // AP: define default max power level to not start with OC;
+	pwr->min_pwrlevel = pdata->num_levels - 1;
 	pwr->thermal_pwrlevel = 0;
 
 	pwr->active_pwrlevel = pdata->init_level;
